83a65d0da244ab6c287577a6fb2e6b3e398e5ea0
secimpalut <secimpalut28@gmail.com>
2021-03-01 22:35:03 +0200

    Third Lesson

---
+++ src/ThirdLesson/Question04.java
@@ -0,0 +1,16 @@
+package ThirdLesson;
+
+import java.util.Arrays;
+
+public class Question04 {
+
+    /*
+    sortNumsAscending([1, 2, 10, 50, 5]) ➞ [1, 2, 5, 10, 50]
+     */
+
+    public static void main(String[] args) {
+
+        int nums [] = {1, 2, 10, 50, 5};
+
+    }
+}
---
+++ src/ThirdLesson/Soru6.java
@@ -0,0 +1,37 @@
+package ThirdLesson;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Soru6 {
+    public static void main(String[] args) {
+        String ulke;
+        Scanner u_girdi = new Scanner(System.in);
+        System.out.print("Yasadiginiz Ulkeyi Griniz...>");
+        ulke = u_girdi.next();
+        ulke = ulke.trim();
+
+        /*
+        ulke =  ulke.substring(0,1) +
+                ulke.substring(1,2).toUpperCase() +
+                ulke.substring(2,ulke.length()-2) +
+                ulke.substring(ulke.length()-2,ulke.length()-1).toUpperCase() +
+                ulke.charAt(ulke.length()-1);
+        */
+        for (int i= 0; i<ulke.length(); i++) {
+            if(i == 1 || i==ulke.length()-2 ) {
+                System.out.print(Character.toUpperCase(ulke.charAt(i)));
+            }
+            else {
+                System.out.print(Character.toLowerCase(ulke.charAt(i)));
+            }
+
+        }
+
+
+     //   System.out.println(ulke);
+
+
+
+    }
+}
